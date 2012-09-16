58a87a0423bec9cf420101304d05f8617259ad7d
Steven Jones <steven.jones@computerminds.co.uk>
2012-09-16 21:49:09 +0100

    Actially allow for failures

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,7 @@ def scan_for_tags(location):
     if os.path.exists(location):
         print('===> Searching for valid tags')
         old_env = env.warn_only
+        env.warn_only = False
         res = local("cd '%s' && git describe --tags --exact-match" % location, capture=True)
         env.warn_only = old_env
         if res.succeeded:
