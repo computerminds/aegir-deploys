8c41c79eb2dc0238c920f92854ce45485a742a0f
Steven Jones <steven.jones@computerminds.co.uk>
2012-09-16 21:54:11 +0100

    Print the results

--- fabfile.py
+++ fabfile.py
@@ -6,13 +6,13 @@ import os
 def scan_for_tags(location):
     if os.path.exists(location):
         print('===> Searching for valid tags')
-        with settings(
-            warn_only=True
-        ):
+
+        # We want to allow for the failure of the git command.
+        with settings(warn_only=True):
             res = local("cd '%s' && git describe --tags --exact-match" % location, capture=True)
 
         if res.succeeded:
-            print('===> Found a tag')
+            print('===> Found a tag: %s' % res)
+            # Now do extra stuff
         else:
             print('===> Could not find a tag')
-
