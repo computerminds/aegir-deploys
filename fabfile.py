e58cd83bad132ec9da80a51cdc2112b457c9638e
Steven Jones <steven.jones@computerminds.co.uk>
2012-09-16 21:43:46 +0100

    Force the command to succeed.

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ import os
 def scan_for_tags(location):
     if os.path.exists(location):
         print('===> Searching for valid tags')
-        res = local("cd '%s' && git describe --tags --exact-match" % location, capture=True)
+        res = local("cd '%s' && git describe --tags --exact-match | true" % location, capture=True)
         if res.succeeded:
             print('===> Found a tag')
         else:
